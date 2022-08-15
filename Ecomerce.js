19a4440841f5ae9ae322ed0fb6990d1d162c6750
Malena Williams <malenanwilliams@gmail.com>
2022-08-15 15:20:39 -0300

    cards_dinamicas

--- Ecomerce.js
+++ Ecomerce.js
@@ -1,4 +1,18 @@
-//Funciones//
+//FUNCIONES//
+function crearProductos() {
+  let ecomerceHtml = document.getElementById("productosHtml");
+  tienda.forEach((producto) => {
+    productosHtml.innerHTML += `<article id="${producto.id}" class="card">
+    <img class="card__img" src="${producto.img}" alt="${producto.nombre}">
+    <h3 class="card__title">${producto.nombre}</h3>
+    <p class="card__info">${producto.descripcion}.</p>
+    <p class="card__info"> Contenido neto: ${producto.contenidoNeto}.</p>
+    <p class="card__price">Precio: $ ${producto.precio}</p>
+    <button class="card__button" id="button_${producto.id}" >Agregar al carrito</button>
+</article>`;
+  });
+}
+
 function muestraCarrito() {
   let comprasHtml = document.getElementById("comprasHtml");
   comprasHtml.innerHTML = ``;
@@ -7,8 +21,8 @@ function muestraCarrito() {
   if (carrito.length > 0)
     carritoHtml.innerText = "Su carrito contiene los siguientes productos: ";
   let nuevaCompra = document.getElementById("comprasHtml");
-  carrito.forEach((producto) => {
-    comprasHtml.innerHTML += `<li>${producto.nombre} $ ${producto.precio}</li>`;
+  carrito.forEach((productos) => {
+    comprasHtml.innerHTML += `<li>${productos.nombre} $ ${productos.precio}</li>`;
   });
   let totalHtml = document.getElementById("totalHtml");
   if (carrito.length > 0)
@@ -41,16 +55,16 @@ function compraRedoxon() {
 }
 
 function compraDeProductos() {
-  let boton_ensure = document.getElementById("boton_ensure");
-  boton_ensure.onclick = compraEnsureAdvance;
-  let boton_creatina = document.getElementById("boton_creatina");
-  boton_creatina.onclick = compraCreatina;
-  let boton_whey = document.getElementById("boton_whey");
-  boton_whey.onclick = compraWheyProtein;
-  let boton_aminototal = document.getElementById("boton_aminototal");
-  boton_aminototal.onclick = compraAminoTotal;
-  let boton_redoxon = document.getElementById("boton_redoxon");
-  boton_redoxon.onclick = compraRedoxon;
+  let boton_ensure = document.getElementById("button_1");
+  button_1.onclick = compraEnsureAdvance;
+  let boton_creatina = document.getElementById("button_2");
+  button_2.onclick = compraCreatina;
+  let boton_whey = document.getElementById("button_3");
+  button_3.onclick = compraWheyProtein;
+  let boton_aminototal = document.getElementById("button_4");
+  button_4.onclick = compraAminoTotal;
+  let boton_redoxon = document.getElementById("button_5");
+  button_5.onclick = compraRedoxon;
 }
 
 function finalizarCompra() {
@@ -67,6 +81,7 @@ function finalizarCompra() {
 }
 
 function ecommerce() {
+  crearProductos();
   compraDeProductos();
 
   let finalizar = document.getElementById("finalizar");
@@ -76,8 +91,9 @@ function ecommerce() {
 //Constructora//
 
 class Productos {
-  constructor(id, nombre, descripcion, contenidoNeto, precio) {
+  constructor(id, img, nombre, descripcion, contenidoNeto, precio) {
     (this.id = id),
+      (this.img = img),
       (this.nombre = nombre.toLocaleUpperCase()),
       (this.descripcion = descripcion),
       (this.contenidoNeto = contenidoNeto),
@@ -89,6 +105,7 @@ class Productos {
 
 const producto1 = new Productos(
   1,
+  "assets/ensure_advance.jpg",
   "Ensure Advance",
   "Suplemento nutricional en polvo sabor banana",
   "400g",
@@ -97,7 +114,8 @@ const producto1 = new Productos(
 
 const producto2 = new Productos(
   2,
-  "Creatina Micronizada ENA",
+  "assets/creatina.jpg",
+  "Creatina ENA",
   "Suplemento deportivo sin sabor",
   "300g",
   6399
@@ -105,6 +123,7 @@ const producto2 = new Productos(
 
 const producto3 = new Productos(
   3,
+  "assets/whey_protein.jpg",
   "Whey Protein",
   "Suplemento deportivo sabor vainilla",
   "930g",
@@ -113,6 +132,7 @@ const producto3 = new Productos(
 
 const producto4 = new Productos(
   4,
+  "assets/aminototal_pulver.jpg",
   "Aminototal Pulver",
   "Aminoacidos esenciales sabor vainilla",
   "1kg",
@@ -121,6 +141,7 @@ const producto4 = new Productos(
 
 const producto5 = new Productos(
   5,
+  "assets/redoxon_vitamina_c.jpg",
   "Redoxon Vitamina C",
   "Suplemento efervescente sabor naranja",
   "30g",
